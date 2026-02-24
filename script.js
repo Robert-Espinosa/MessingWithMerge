9f632edaebfb1a1d01826633fc8bd7647bf9fe02
Robert Espinosa <75014892+Robert-Espinosa@users.noreply.github.com>
2026-02-24 15:19:51 -0500

    conflict A with changed title

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const list = document.getElementById("preview");
-let title ="javaScript-title"
+let title ="javaScript-title- conflict A";
 
 const elements = {
   darkStats: document.getElementById("darkStats"),
